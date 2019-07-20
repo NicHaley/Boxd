33272721f091f76ee2f104abf2931dc6ac7b1274
NicHaley <nicholaswilliamhaley@gmail.com>
2019-07-20 12:55:02 -0400

    Initial Commit

---
+++ Boxd/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Boxd
+//
+//  Created by Nicholas Haley on 2019-07-20.
+//  Copyright © 2019 Nicholas Haley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
